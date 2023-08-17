53e5538ce719fec420f20314b370d7940c113d16
Anka babu Lanka <ankababu1911@gmail.com>
2023-08-17 14:20:19 +0530

    Added gateway project to project security to all API services using ocelot

---
+++ Mango/Mango.GatewaySolution/Program.cs
@@ -0,0 +1,13 @@
+using Mango.GatewaySolution.Extensions;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Values;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.AddAppAuthentication();
+builder.Configuration.AddJsonFile("Ocelot.json",optional:false,reloadOnChange:true);
+builder.Services.AddOcelot(builder.Configuration);
+var app = builder.Build();
+app.MapGet("/", () => "Hello World!");
+app.UseOcelot().GetAwaiter().GetResult();
+app.Run();
